50c4e6c562355900bc7c8f15137c6756b9276ad6
PramodSuthar <67489670+PramodSuthar@users.noreply.github.com>
2021-09-12 12:02:54 -0300

    Update books.js

--- src/books.js
+++ src/books.js
@@ -4,15 +4,15 @@ export const books = [
         img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200,200_.jpg',
         title: 'I Love you the Moon and Back',
         author: 'Amelia Bundlewoman'
-    },
-
+    }
+    ,
     {
         id: 2,
         img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200,200_.jpg',
         title: 'Our Class is a family',
         author: 'Shannon Olsen'
-    },
-
+    }
+    ,
     {
         id: 3,
         img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200,200_.jpg',
